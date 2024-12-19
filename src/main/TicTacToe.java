c91b95b5eacebe42ee798b811d695dbce9de7997
Daniel Polgun <danielpolgun@gmail.com>
2024-12-19 18:08:23 -0500

    First iteration. Board printed to system. Choice goes to correct spot.

--- src/main/TicTacToe.java
+++ src/main/TicTacToe.java
@@ -4,6 +4,8 @@ import java.util.Scanner;
 
 public class TicTacToe {
 
+    private static int[][] taken = new int[3][3];
+
     public static void printGameBoard(char[][] board){
 
         //Prints out game board
@@ -15,7 +17,85 @@ public class TicTacToe {
         }
     }
 
+    public static void placePiece(char[][] board, int choice, String user){
 
+        char symbol = 'X';
+
+        if(user.equals("player"))
+            symbol = 'X';
+        else
+            symbol = 'O';
+
+        switch(choice){
+            case 1:
+                if(taken[0][0] == 0)
+                    board[0][0] = symbol;
+                else
+                    System.out.println("Spot taken");
+                taken[0][0] = 1;
+                break;
+            case 2:
+                if(taken[0][1] == 0)
+                    board[0][2] = symbol;
+                else
+                    System.out.println("Spot taken");
+                taken[0][1] = 1;
+                break;
+            case 3:
+                if(taken[0][2] == 0)
+                    board[0][4] = symbol;
+                else
+                    System.out.println("Spot taken");
+                taken[0][2] = 1;
+                break;
+            case 4:
+                if(taken[1][0] == 0)
+                    board[2][0] = symbol;
+                else
+                    System.out.println("Spot taken");
+                taken[1][0] = 1;
+                break;
+            case 5:
+                if(taken[1][1] == 0)
+                    board[2][2] = symbol;
+                else
+                    System.out.println("Spot taken");
+                taken[1][1] = 1;
+                break;
+            case 6:
+                if(taken[1][2] == 0)
+                    board[2][4] = symbol;
+                else
+                    System.out.println("Spot taken");
+                taken[1][2] = 1;
+                break;
+            case 7:
+                if(taken[2][0] == 0)
+                    board[4][0] = symbol;
+                else
+                    System.out.println("Spot taken");
+                taken[2][0] = 1;
+                break;
+            case 8:
+                if(taken[2][1] == 0)
+                    board[4][2] = symbol;
+                else
+                    System.out.println("Spot taken");
+                taken[2][1] = 1;
+                break;
+            case 9:
+                if(taken[2][2] == 0)
+                    board[4][4] = symbol;
+                else
+                    System.out.println("Spot taken");
+                taken[2][2] = 1;
+                break;
+        }
+    }
+
+    public static void checkSpot(char[][] board, int choice){
+
+    }
 
     public static void main(String[] args){
 
@@ -27,47 +107,41 @@ public class TicTacToe {
 
         printGameBoard(board);
 
-        System.out.println("Choose spot on grid by typing a number 1 - 9");
         Scanner k = new Scanner(System.in);
-        int choice = k.nextInt();
 
-        //Checks for valid number
-        while(choice < 1 || choice > 9){
-            System.out.println("Invalid Number\nEnter a number between 1 - 9");
-            choice = k.nextInt();
+        int turn = 0;
+        boolean boardFull = false;
+        while(!boardFull){
+            System.out.println("Choose spot on grid by typing a number 1 - 9");
+            int choice = k.nextInt();
+
+            //Checks for valid number
+            while(choice < 1 || choice > 9){
+                System.out.println("Invalid Number\nEnter a number between 1 - 9");
+                choice = k.nextInt();
+            }
+            if(turn == 0){
+                placePiece(board, choice, "player");
+                turn = 1;
+            }
+            else if (turn == 1) {
+                placePiece(board, choice, "cpu");
+                turn = 0;
+            }
+            printGameBoard(board);
+            boardFull = true;
+            for(int[] row : taken){
+                for(int c : row){
+                    if (c == 0) {
+                        boardFull = false;
+                        break;
+                    }
+                }
+            }
         }
 
-        switch(choice){
-            case 1:
-                board[0][0] = 'X';
-                break;
-            case 2:
-                board[0][2] = 'X';
-                break;
-            case 3:
-                board[0][4] = 'X';
-                break;
-            case 4:
-                board[2][0] = 'X';
-                break;
-            case 5:
-                board[2][2] = 'X';
-                break;
-            case 6:
-                board[2][4] = 'X';
-                break;
-            case 7:
-                board[4][0] = 'X';
-                break;
-            case 8:
-                board[4][2] = 'X';
-                break;
-            case 9:
-                board[4][4] = 'X';
-                break;
 
-        }
-        printGameBoard(board);
+
 
 
     }
